14a1f7c086ace1a0c9ce4855b840331dca51dff8
Calebfelix0125 <nathanaelfelix1009@gmail.com>
2024-09-23 23:14:38 +0700

    Dashboard display revision,  javascript

---
+++ script.js
@@ -0,0 +1,42 @@
+// function showTab (tabId){
+//     document.querySelectorAll('.home-center').forEach(div => {
+//         selection.style.display = 'none';
+//     })
+//     document.getElementById(tabId).style.display = 'block';
+// }
+
+  
+// Navbar colouring and accessing
+$( "#navbar" ).selectable();
+
+$(".navbar").click(function(event) {
+
+if ($(this).is('a')) {
+      event.preventDefault(); 
+}
+    // event.preventDefault();
+$(".navbar").removeClass("selected");  // Remove 'selected' class from all items
+ $(this).addClass("selected");  // Add 'selected' class to clicked item
+});
+
+
+// Display the news highlight
+$(document).ready(function() {
+   let images = $('.news-img');
+   images.not(':first').hide();
+   
+   let current = 0;
+
+   function startImageTrans() {
+       setInterval(fade, 5000);
+   }
+
+   function fade() {
+       $(images[current]).fadeOut(750, function() {
+           current = (current + 1) % images.length;
+           $(images[current]).fadeIn(750);
+       });
+   }
+
+   startImageTrans();
+});
